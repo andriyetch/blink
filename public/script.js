6238b6b228dd26e2f4ad49c4416fd0e308dc4bb5
andriyetch <andriy.etcheverry@gmail.com>
2022-05-19 14:58:38 +0400

    update

---
+++ public/script.js
@@ -0,0 +1,36 @@
+import 'https://unpkg.com/@microblink/blinkid-in-browser-sdk@5.16.0/ui/dist/blinkid-in-browser/blinkid-in-browser.esm.js';
+
+const blinkId = document.querySelector('blinkid-in-browser');
+
+blinkId.addEventListener('fatalError', ev => {
+  console.log('fatalError', ev.detail);
+});
+
+blinkId.addEventListener('ready', ev => {
+  console.log('ready', ev.detail);
+});
+
+blinkId.addEventListener('scanError', ev => {
+  console.log('scanError', ev.detail);
+});
+
+blinkId.addEventListener('scanSuccess', ev => {
+  console.log('scanSuccess', ev.detail);
+  
+  const results = ev.detail.recognizer;
+  const firstName = results.firstName || results.mrz.secondaryID;
+  const lastName = results.lastName || results.mrz.primaryID;
+  const dateOfBirth = {
+    year: results.dateOfBirth.year || results.mrz.dateOfBirth.year,
+    month: results.dateOfBirth.month || results.mrz.dateOfBirth.month,
+    day: results.dateOfBirth.day || results.mrz.dateOfBirth.day
+  }
+
+  alert(
+    `Hello, ${ firstName } ${ lastName }!\n You were born on ${ dateOfBirth.year }-${ dateOfBirth.month }-${ dateOfBirth.day }.`
+  );
+});
+
+blinkId.addEventListener('feedback', ev => {
+  console.log('feedback', ev.detail);
+});
